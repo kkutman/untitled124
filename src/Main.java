25596a25ae204e046b284f05420e01eba82783ae
КМ <kkutman18@gmail.com>
2023-01-03 16:52:49 +0600

    new project

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("kutman");
         System.out.println("kutman2");
+        System.out.println("kutman2");
     }
 }
