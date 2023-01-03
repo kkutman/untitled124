9eef4266a1b6a68daa2282d4c5b30a07fa077731
КМ <kkutman18@gmail.com>
2023-01-03 17:44:22 +0600

    new

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("kutman");
-        System.out.println("kutman2");
-        System.out.println("kutman2");
-
-
-        System.out.println("dsds");
-        System.out.println("dsds");
-
-
-
-
-        System.out.println("kutman");
-        System.out.println("kutman2");
-        System.out.println("kutman2");
-
-
-        System.out.println("dsds");
-        System.out.println("dsds");
-
+        System.out.println("hello world");
 
     }
 }
