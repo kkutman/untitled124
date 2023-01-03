eb38a9c7193b745c89c05977e9fffb69671ca58a
КМ <kkutman18@gmail.com>
2023-01-03 18:41:06 +0600

    new

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("dssdd1sd");
+        System.out.println("dssdd1sd");
 
     }
 }
